a39debdaadb253923b3d3a1ffd49c74acc74e682
Ali Akkas <aliakkas006@gmail.com>
2021-10-13 23:49:18 +0600

    experts data added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Card, Col } from 'react-bootstrap';
 import './Service.css'
 
 const Service = ({ service }) => {
     const { name, description, price, time, img } = service;
     
     return (
-        <div className="service">
-            <img src={img} alt="" />
-            <h2> {name} </h2>
-            <p> {description} </p>
-            <h5>Duration: {time}hour </h5>
-            <h3>Price: {price} </h3>
-        </div>
+        <>
+            <Col>
+                <Card className="h-100">
+                    <Card.Img variant="top" src={img} />
+                    <Card.Body>
+                        <Card.Title> {name} </Card.Title>
+                        <Card.Text>
+                            {description}
+                        </Card.Text>
+                        <h5>Duration: {time} </h5>
+                        <h4 className="text-warning">Price: {price} </h4>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </>
     );
 };
 
